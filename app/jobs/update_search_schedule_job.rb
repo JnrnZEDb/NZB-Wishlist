53c5b1222ec62e23997f3315076f5104f6c5aa23
Michael Tighe <striderIIDX@gmail.com>
2015-04-27 10:56:29 -0500

    Update job with placeholder

--- app/jobs/update_search_schedule_job.rb
+++ app/jobs/update_search_schedule_job.rb
@@ -2,7 +2,7 @@ class UpdateSearchScheduleJob < ActiveJob::Base
     queue_as :default
 
     def perform(*args)
-        domain = 'iroha'
+        domain = '[YOUR DOMAIN]' # Put the domain from your config/deploy.rb here
         system "bundle exec whenever --update-crontab #{domain}_#{Rails.env}"
     end
 
